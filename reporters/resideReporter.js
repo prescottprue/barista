3592dc9e7e8c355d725e08101cc9de913b3c8086
Stephen Saunders <saunders.stephen@gmail.com>
2018-07-20 17:11:53 -0700

    reporter changes

--- reporters/resideReporter.js
+++ reporters/resideReporter.js
@@ -3,12 +3,50 @@ const path = require('path')
 const serviceAccountPath = path.join(process.cwd(), 'serviceAccount.json')
 const serviceAccount = require(serviceAccountPath)
 const admin = require('firebase-admin')
+const moment = require('moment');
+const { reduce, set, isFunction, isUndefined, forEach, get } = require('lodash')
+
+const passed = 'passed'
+const failed = 'failed'
+const pending = 'pending'
+
+const omitList = [
+  'fn',
+  'async',
+  'sync',
+  '_timeout',
+  '_slow',
+  '_enableTimeouts',
+  'timedOut',
+  '_trace',
+  '_retries',
+  '_currentRetry',
+  'parent'
+]
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: `https://reside-dev-saunders.firebaseio.com`
 })
 
+function sanitizeTest(test, propList) {
+  forEach(omitList, prop => {
+    delete test[prop]
+  })
+  return reduce(
+    test,
+    (acc, value, key) => {
+      if (isFunction(value)) {
+        return acc
+      }
+      if (isUndefined(value)) {
+        return set(acc, key, null)
+      }
+      return set(acc, key, value)
+    },
+    {}
+  )
+}
 function writeToDatabase(dbRef, data) {
   return dbRef.update(data).catch(err => {
     console.log(`error writing data to Firebase at path: ${dbRef.path}`)
@@ -19,71 +57,85 @@ function writeToDatabase(dbRef, data) {
 module.exports = MyReporter
 
 function MyReporter(runner) {
+  const testJobKey = process.env.TEST_JOB_KEY || 'testJobKey'
   const dbRef = admin
     .database()
     .ref('test_results_data')
-    .push()
+    .child(testJobKey)
+
+  const metaRef = admin
+    .database()
+    .ref('test_results_meta')
+    .child(testJobKey)
 
   mocha.reporters.Base.call(this, runner)
-  let self = this
-  let passes = 0
-  let failures = 0
-  let currentTestIndex = 0
-  let total = runner.total
 
-  function getCurrentTestRef() {
-    return dbRef.child('tests').child(currentTestIndex)
+  let currentSuiteTitle = ''
+
+  function getTestRef(test) {
+    return dbRef.child(currentSuiteTitle).child(test.id)
   }
 
-  runner.on('start', function(test) {
-    console.log('Start test run. Total tests: %s', total)
-    console.log('******************Test - start', test)
-    return writeToDatabase(dbRef.child('start'), { total })
+  runner.on('start', function() {
+    writeToDatabase(metaRef, { status: pending, [pending]: true })
+  })
+  runner.on('end', async function() {
+    const { passes, failures, end } = this.stats
+    console.log('Test run end: %d/%d', passes, passes + failures)
+    await metaRef
+      .child('stats')
+      .once('value')
+      .then(testJobMetaStatsSnap => {
+        const existingStats = testJobMetaStatsSnap.val()
+        console.log('existingStats', existingStats)
+        const newStats = reduce(
+          this.stats,
+          (stats, value, key) => {
+            if (key === 'start') {
+              return stats
+            }
+            if (key === 'end') {
+              return set(stats, key, end)
+            }
+            return set(stats, key, value + get(existingStats, key, 0))
+          },
+          {}
+        )
+        writeToDatabase(metaRef.child('stats'), newStats)
+          .then(function() {
+            const hasFailures = failures > 0 || get(existingStats, failures, 0) > 0
+            console.log('hasFailures', hasFailures);
+            if (hasFailures) {
+              return writeToDatabase(metaRef, { status: failed, pending: false })
+            } else {
+              return writeToDatabase(metaRef, { status: passed, pending: false })
+            }
+          })
+        })
+  })
+
+  // when the new test file is loaded
+  runner.on('suite', function(suite) {
+    if (suite.title) {
+      currentSuiteTitle = suite.title
+    }
+  })
+  runner.on('suite end', function() {
+    currentSuiteTitle = ''
+  })
+  runner.on('test', function(test) {
+    writeToDatabase(getTestRef(test), { state: 'pending' })
+    writeToDatabase(metaRef, { pending: true })
+  })
+  runner.on('test end', function(test) {
+    writeToDatabase(getTestRef(test), sanitizeTest(test))
   })
 
   runner.on('pass', function(test) {
-    console.log('******************Test - pass', test)
-    const passRef = getCurrentTestRef().child('pass')
-    passes++
-    currentTestIndex++
-    return writeToDatabase(passRef, clean(test))
+    writeToDatabase(getTestRef(test), { state: 'pass' })
   })
-
   runner.on('fail', function(test, err) {
-    console.log('******************Test - fail', test)
-    const failRef = getCurrentTestRef().child('fail')
-    failures++
-    currentTestIndex++
-    console.log('Test fail: %s -- error: %s', test.fullTitle(), err.message)
-    return writeToDatabase(failRef, clean(test))
-  })
-
-  runner.once('end', function() {
-    console.log('Test run end: %d/%d', passes, passes + failures)
-    writeToDatabase(dbRef.child('end'), {
-      failures,
-      passes,
-      total,
-      stats: self.stats
-    })
+    writeToDatabase(getTestRef(test), { state: 'failed' })
+    console.log('Test fail: %s -- error: %s', test.title, err.message)
   })
 }
-
-/**
- * Return a plain-object representation of `test`
- * free of cyclic properties etc.
- *
- * @api private
- * @param {Object} test
- * @return {Object}
- */
-function clean(test) {
-  return {
-    title: test.title,
-    fullTitle: test.fullTitle(),
-    duration: test.duration,
-    currentRetry: test.currentRetry()
-  }
-}
-
-mocha.utils.inherits(MyReporter, mocha.reporters.Spec)
