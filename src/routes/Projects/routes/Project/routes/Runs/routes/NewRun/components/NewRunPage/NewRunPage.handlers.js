786a82964aac277a9ac6e93b92e89f251962b7ce
Scott Prue <scott@residebrokerage.com>
2018-08-17 12:47:08 -0700

    Push to test_runs_meta
    Co-Authored-By: Stephen Saunders <saunders.stephen@gmail.com>

--- src/routes/Projects/routes/Project/routes/Runs/routes/NewRun/components/NewRunPage/NewRunPage.handlers.js
+++ src/routes/Projects/routes/Project/routes/Runs/routes/NewRun/components/NewRunPage/NewRunPage.handlers.js
@@ -10,7 +10,13 @@ export function startTestRun({ firebase, params: { projectId } }) {
   const baristaProjectId = 'EJrWyFiygj6y3g2FfZh7'
   const baristaEnvironment = 'B2rKSXXOtyF5JBzXeI6k'
   return () => {
+    const pushRef = firebase.pushWithMeta('test_runs_meta', {
+      projectId: baristaProjectId,
+      environment: baristaEnvironment
+    })
+    const pushKey = pushRef.key
     return firebase.push('requests/callRunner', {
+      jobRunKey: pushKey,
       projectId: baristaProjectId,
       environment: baristaEnvironment
     })
