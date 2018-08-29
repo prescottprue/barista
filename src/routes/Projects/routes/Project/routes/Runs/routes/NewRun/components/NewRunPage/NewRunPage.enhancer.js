9d2843049599cb3e20ada5237c5a711d1c51a646
Stephen Saunders <saunders.stephen@gmail.com>
2018-08-29 11:40:10 -0700

    CA-3234 Add buildId to callRunner Request

--- src/routes/Projects/routes/Project/routes/Runs/routes/NewRun/components/NewRunPage/NewRunPage.enhancer.js
+++ src/routes/Projects/routes/Project/routes/Runs/routes/NewRun/components/NewRunPage/NewRunPage.enhancer.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types'
 import { compose } from 'redux'
+import { connect } from 'react-redux'
 import { withHandlers, setPropTypes, withProps } from 'recompose'
-import { withFirebase } from 'react-redux-firebase'
+import { withFirebase, firestoreConnect } from 'react-redux-firebase'
 import { withStyles } from '@material-ui/core'
 import * as handlers from './NewRunPage.handlers'
-import { paths } from 'constants'
+import { CONTAINER_BUILDS_META_PATH, paths } from 'constants'
+import { getMostRecentBuildId } from 'selectors'
 import styles from './NewRunPage.styles'
 
 export default compose(
@@ -23,6 +25,20 @@ export default compose(
   withProps(({ params: { projectId } }) => ({
     projectId,
     runsPagePath: `${paths.list}/${projectId}/${paths.runs}`
+  })),
+  // ensure that we are getting the the build object of the last build image
+  firestoreConnect(({ projectId }) => [
+    {
+      collection: CONTAINER_BUILDS_META_PATH,
+      orderBy: ['finishTime', 'desc'],
+      where: ['projectId', '==', projectId],
+      limit: 1,
+      storeAs: `mostRecentBuild-${projectId}`
+    }
+  ]),
+  // pass along the build id
+  connect((state, props) => ({
+    buildId: getMostRecentBuildId(state, props)
   })),
   // add handlers as props
   withHandlers(handlers),
