70ab9bde5127f2f86de687383ee8166c1f76e323
Stephen Saunders <saunders.stephen@gmail.com>
2018-09-05 13:41:24 -0700

    use IconFromStatus component

--- src/routes/Projects/routes/Project/routes/Runs/components/RunMetaItem/RunMetaItem.js
+++ src/routes/Projects/routes/Project/routes/Runs/components/RunMetaItem/RunMetaItem.js
@@ -8,29 +8,8 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Tooltip from '@material-ui/core/Tooltip'
 import LinearProgress from '@material-ui/core/LinearProgress'
-import {
-  Error as ErrorIcon,
-  CheckCircle,
-  Warning,
-  ArrowForward as GoTo,
-  Replay as Rerun
-} from '@material-ui/icons'
-
-function iconFromStatus({ status, runResult, classes }) {
-  switch (status) {
-    case 'failed':
-      return <ErrorIcon color="error" />
-    case 'passed':
-      return <CheckCircle className={classes.pass} />
-    case 'pending':
-      if (runResult === 'failed') {
-        return <ErrorIcon color="error" />
-      }
-      return <Rerun color="disabled" className={classes.pending} />
-    default:
-      return <Warning />
-  }
-}
+import { ArrowForward as GoTo, Replay as Rerun } from '@material-ui/icons'
+import IconFromStatus from 'components/IconFromStatus'
 
 export const RunMetaItem = ({
   classes,
@@ -57,7 +36,7 @@ export const RunMetaItem = ({
         root: classes.summaryRoot
       }}>
       <Typography align="center" variant="body1" className={classes.data}>
-        {iconFromStatus({ status, classes, runResult })}
+        <IconFromStatus status={status} runResult={runResult} />
       </Typography>
       <Tooltip title="Run Id">
         <Typography align="center" variant="body1" className={classes.data}>
@@ -149,11 +128,8 @@ RunMetaItem.propTypes = {
   runDetailPath: PropTypes.string.isRequired, // from enhancer (withProps)
   classes: PropTypes.object.isRequired, // from enhancer (withStyles)
   reRunJob: PropTypes.func.isRequired, // from enhancer (withHandlers)
-  formattedStart: PropTypes.string.isRequired, // from enhancer (withProps)
   startedAtToolTip: PropTypes.string.isRequired, // from enhancer (withProps)
   tests: PropTypes.number.isRequired, // from enhancer (flattenProp('stats'))
-  showProgress: PropTypes.bool.isRequired, // from enhancer (withProps)
-  allTests: PropTypes.number.isRequired // from enhancer (flattenProp('runMeta'))
   createdAtTooltip: PropTypes.string.isRequired // from enhancer (withProps)
 }
 
--- src/routes/Projects/routes/Project/routes/Runs/components/RunMetaItem/RunMetaItem.styles.js
+++ src/routes/Projects/routes/Project/routes/Runs/components/RunMetaItem/RunMetaItem.styles.js
@@ -23,18 +23,6 @@ export default theme => ({
   dateWords: {
     textAlign: 'left'
   },
-  fail: {
-    color: 'red'
-  },
-  pass: {
-    color: 'green'
-  },
-  pending: {
-    color: 'grey',
-    '-webkit-animation': 'spin 4s linear infinite',
-    '-moz-animation': 'spin 4s linear infinite',
-    animation: 'spin 4s linear infinite'
-  },
   buttonContainer: {
     display: 'grid',
     gridTemplateColumns: 'repeat(2, 1fr)',
@@ -54,22 +42,5 @@ export default theme => ({
     gridColumnStart: 1,
     gridColumnEnd: 9,
     marginTop: '1rem'
-  },
-  // Causes "Warning: [JSS] Unknown at-rule @-moz-keyframes spin"
-  // '@-moz-keyframes spin': {
-  //   '100%': {
-  //     '-moz-transform': 'rotate(-360deg)'
-  //   }
-  // },
-  // '@-webkit-keyframes spin': {
-  //   '100%': {
-  //     '-webkit-transform': 'rotate(-360deg)'
-  //   }
-  // },
-  '@keyframes spin': {
-    '100%': {
-      '-webkit-transform': 'rotate(-360deg)',
-      transform: 'rotate(-360deg)'
-    }
   }
 })
