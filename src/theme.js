04dbd5f3c7c65c24d69917226f2992b1421dc952
Stephen Saunders <saunders.stephen@gmail.com>
2018-09-07 14:29:51 -0700

    add theme colors

--- src/theme.js
+++ src/theme.js
@@ -8,6 +8,19 @@ export default {
     },
     success: {
       main: '#51BF87'
-    }
+    },
+    grey0: '#3B3D3D',
+    grey1: '#656E70',
+    grey2: '#ABB1B3',
+    grey3: '#D5D5D5',
+    grey4: '#F5F5F5',
+    white: '#FFFFFF'
+  },
+  baseIcon: {
+    height: '1rem',
+    width: '1rem',
+    marginRight: '.25rem',
+    position: 'relative',
+    top: '-1px'
   }
 }
