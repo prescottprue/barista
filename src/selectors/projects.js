0eee24609788873dc0ad8350dd2036b0b30c6c14
prescottprue <sprue.dev@gmail.com>
2018-09-19 16:33:05 -0700

    Support created and collaborator projects.

--- src/selectors/projects.js
+++ src/selectors/projects.js
@@ -1,12 +1,35 @@
-import { get } from 'lodash'
+import { get, uniqBy } from 'lodash'
+import { createSelector } from 'reselect'
 
 /**
  * @param {Object} state - Redux state (from connect)
  * @param {Object} props - Component props
  */
-export function getOrderedProjects(state, props) {
+function getOrderedMyProjects(state, props) {
+  return get(state, 'firestore.ordered.myProjects')
+}
+
+/**
+ * @param {Object} state - Redux state (from connect)
+ * @param {Object} props - Component props
+ */
+function getOrderedCollabProjects(state, props) {
   return get(state, 'firestore.ordered.projects')
 }
+
+/**
+ * @param {Object} state - Redux state (from connect)
+ * @param {Object} props - Component props
+ */
+export const getOrderedProjects = createSelector(
+  [getOrderedMyProjects, getOrderedCollabProjects],
+  (myProjects, collabProjects) => {
+    if (myProjects && myProjects.length) {
+      return uniqBy(myProjects.concat(collabProjects), 'id')
+    }
+    return collabProjects
+  }
+)
 
 /**
  * @param {Object} state - Redux state (from connect)
